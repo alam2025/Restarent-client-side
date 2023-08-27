1d74e67c3476cb44a20f91bd5201b9d6cff195df
alam2025 <alam13100852@gmail.com>
2023-08-27 16:26:27 +0600

    url change

--- src/CustomHooks/URL/UseUrl.jsx
+++ src/CustomHooks/URL/UseUrl.jsx
@@ -1,5 +1,6 @@
 const useUrl=()=>{
-      const url="http://localhost:3000";
+      // const url="http://localhost:3000";
+      const url="https://restaurent-backend-ivory.vercel.app";
       return [url]
 }
 export default useUrl
