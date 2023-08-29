e9864822b3534e801698fac47067bca755e287e5
alam2025 <alam13100852@gmail.com>
2023-08-29 23:49:49 +0600

    ok

--- src/layout/Dashboard/Report/ReportTable.jsx
+++ src/layout/Dashboard/Report/ReportTable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DataTable from 'react-data-table-component';
 
 const ReportTable = ({ report }) => {
-      
+      // ds
       const columns = [
             
             {
