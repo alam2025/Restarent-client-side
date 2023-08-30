4a71100092dae32bf568e4c199ef0c7b8ccdfa61
Ekram Hossain <ekramhossain117@gmail.com>
2023-08-30 22:03:25 +0600

    back added

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -3,7 +3,7 @@ import restaurent from '../../public/restaurent.json'
 import Lottie from 'react-lottie';
 import { Controller, useForm } from 'react-hook-form';
 import { AuthContext } from '../providers/AuthoProvider';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 const Login = () => {
       const { login } = useContext(AuthContext);
       const navigate = useNavigate()
@@ -63,6 +63,15 @@ const Login = () => {
                               </div>
                         </div>
                   </div>
+            <div className='flex items-center justify-center'>
+            <Link to="/">
+                              <button
+                                    className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors focus:ring focus:ring-blue-300"
+                              >
+                                    {'<----'} Back
+                              </button>
+                        </Link>
+            </div>
             </div>
       );
 };
