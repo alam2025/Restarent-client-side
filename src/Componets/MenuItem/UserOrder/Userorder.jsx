ec0fdf8bf9695edc0757bb0fbe776b4cb75e1a4e
alam2025 <alam13100852@gmail.com>
2023-09-19 23:12:27 +0600

    pull

--- src/Componets/MenuItem/UserOrder/Userorder.jsx
+++ src/Componets/MenuItem/UserOrder/Userorder.jsx
@@ -86,7 +86,7 @@ const Userorder = () => {
                         });
                         deleteShoppingCart();
                         const goToHone=()=>{
-                              location.reload()
+                              
                               navigate('/')
                         }
 
